ad87c05718c77ae357410c2eaa7782232a3f34a1
Alex <aleksandr.andrejev678@gmail.com>
2023-02-06 20:25:06 +0200

    6.02.23

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-console.log('arr' + "- object");
-console.log(4  +"5");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-let incr = 10,
-    decr = 10;
+};
 
-    //++incr;
-   // --decr;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?','');
 
-    console.log(incr++);
-    console.log(decr--);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-    const isChecked = true;
-            isClose = true;
-
-    
+console.log(personalMovieDB);
